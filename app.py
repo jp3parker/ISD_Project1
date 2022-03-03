c5f1b0652933cf80b85d0abd426b975fcf9e1073
jhp59 <jhp59@txstate.edu>
2022-03-03 16:58:04 -0600

    established project1 page structure and movie table works on Movie_Page.html

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, json
+import codecs
+
+app = Flask(__name__)
+
+with codecs.open("static/data/movies.json", "r", encoding="utf-8") as moviesFile:
+    data = moviesFile.read();
+
+
+@app.route('/')
+def page_example():  # put application's code here
+    return render_template("Intro.html")
+
+
+@app.route('/Movie_Page.html')
+def page_2():
+    return render_template("Movie_Page.html", MOVIES=data)
+
+
+@app.route('/Actors_Page.html')
+def page_3():
+    return render_template("Actors_Page.html")
+
+
+if __name__ == '__main__':
+    app.run()
