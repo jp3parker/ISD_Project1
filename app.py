f14e75cec1885aebfdf6ce6a67e567f5845d45be
jhp59 <jhp59@txstate.edu>
2022-03-07 01:04:46 -0600

    actors page now has table

--- app.py
+++ app.py
@@ -5,23 +5,24 @@ import codecs
 
 app = Flask(__name__)
 
+
 with codecs.open("static/data/movies.json", "r", encoding="utf-8") as moviesFile:
     data = moviesFile.read();
 
 
 @app.route('/')
-def page_example():  # put application's code here
+def homePage():  # put application's code here
     return render_template("Intro.html")
 
 
 @app.route('/Movie_Page.html')
-def page_2():
+def moviesPage():
     return render_template("Movie_Page.html", MOVIES=data)
 
 
 @app.route('/Actors_Page.html')
-def page_3():
-    return render_template("Actors_Page.html")
+def actorsPage():
+    return render_template("Actors_Page.html", MOVIES=data)
 
 
 if __name__ == '__main__':
